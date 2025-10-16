f71a16da1148e83c1286872bfb5934b85570a258
Yonatan Peters <yonipeters7@gmail.com>
2025-10-16 09:21:01 -0400

    Analyzing Data Modules

--- index.js
+++ index.js
@@ -4,3 +4,20 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+require('datejs');
+
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+  
+  for (let array of args) {
+    combinedObject.users.push(...array);
+  }
+  
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+  
+  return combinedObject;
+}
+
+
